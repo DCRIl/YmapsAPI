d54c0ee91c08522098a7a16c56ed7cf13dea0cae
DCRIl <kelehsaev.2007@gmail.com>
2024-02-05 19:26:10 +0300

    Добавил поле для ввода и кнопку

--- main.py
+++ main.py
@@ -5,6 +5,42 @@ import pygame
 import requests
 
 
+BLACK = "black"
+
+
+class TextInput:
+    def __init__(self, x, y, width, height, color=(255, 255, 255)):
+        self.rect = pygame.Rect(x, y, width, height)
+        self.color = color
+        self.text = ""
+        self.font = pygame.font.Font(None, 36)
+
+    def draw(self):
+        pygame.draw.rect(screen, self.color, self.rect)
+        text_surface = self.font.render(self.text, True, BLACK)
+        screen.blit(text_surface, (self.rect.x + 5, self.rect.y + 5))
+
+    def add_text(self, event):
+        if event.key == pygame.K_BACKSPACE:
+            self.text = self.text[:-1]
+        else:
+            self.text += event.unicode
+
+
+class Button:
+    def __init__(self, x, y, width, height, color, text):
+        self.rect = pygame.Rect(x, y, width, height)
+        self.color = color
+        self.text = text
+
+    def draw(self):
+        pygame.draw.rect(screen, self.color, self.rect)
+        font = pygame.font.Font(None, 36)
+        text = font.render(self.text, True, BLACK)
+        text_rect = text.get_rect(center=self.rect.center)
+        screen.blit(text, text_rect)
+
+
 def get_map(lan, lat, spn, type_map):
     map_request = f"http://static-maps.yandex.ru/1.x/?ll={lat},{lan}&spn={spn[0]},{spn[1]}&l={type_map}"
     response = requests.get(map_request)
