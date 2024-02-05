07a9f41ce7a0807adf990c20180862ff79b2e512
DCRIl <kelehsaev.2007@gmail.com>
2024-02-05 18:45:05 +0300

    баг в отображении изменения координат

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import sys
 import pygame
 import requests
 
-lan, lat, spn = 43.024616, 44.681771, [0.002, 0.002]
+lan, lat, spn = 43.024616, 44.681771, [0.02, 0.02]
 type_map = "map"
 
 map_request = f"http://static-maps.yandex.ru/1.x/?ll={lan},{lat}&spn={spn[0]},{spn[1]}&l={type_map}"
@@ -24,12 +24,34 @@ pygame.init()
 screen = pygame.display.set_mode((600, 450))
 screen.blit(pygame.image.load(map_file), (0, 0))
 pygame.display.flip()
-while pygame.event.wait().type != pygame.QUIT:
+run = True
+while run:
     for event in pygame.event.get():
-        if event.type == pygame.K_UP:
-            lan -= 0.01
-        if event.type == pygame.K_DOWN:
-            lan += 0.01
+        if event.type == pygame.QUIT:
+            run = False
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_UP:
+                lan -= 1
+            if event.key == pygame.K_DOWN:
+                lan += 1
+            if event.key == pygame.K_RIGHT:
+                lat -= 1
+            if event.key == pygame.K_LEFT:
+                lat += 1
+    map_request = f"http://static-maps.yandex.ru/1.x/?ll={lan},{lat}&spn={spn[0]},{spn[1]}&l={type_map}"
+    response = requests.get(map_request)
+
+    if not response:
+        print("Ошибка выполнения запроса:")
+        print(map_request)
+        print("Http статус:", response.status_code, "(", response.reason, ")")
+        sys.exit(1)
+
+    map_file = "map.png"
+    with open(map_file, "wb") as file:
+        file.write(response.content)
+    screen.fill("black")
+    screen.blit(pygame.image.load(map_file), (0, 0))
 pygame.quit()
 
 os.remove(map_file)
