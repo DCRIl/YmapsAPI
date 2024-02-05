0a153a666f63d754f3999e6385e74b0291d6c0fd
DCRIl <kelehsaev.2007@gmail.com>
2024-02-05 18:55:33 +0300

    баг в отображении изменения координат, вынес в отдельную функцию получение карты

--- main.py
+++ main.py
@@ -4,21 +4,26 @@ import sys
 import pygame
 import requests
 
+
+def get_map(lan, lat, spn, type_map):
+    map_request = f"http://static-maps.yandex.ru/1.x/?ll={lan},{lat}&spn={spn[0]},{spn[1]}&l={type_map}"
+    response = requests.get(map_request)
+
+    if not response:
+        print("Ошибка выполнения запроса:")
+        print(map_request)
+        print("Http статус:", response.status_code, "(", response.reason, ")")
+        sys.exit(1)
+
+    with open(map_file, "wb") as file:
+        file.write(response.content)
+
+map_file = "map.png"
+
 lan, lat, spn = 43.024616, 44.681771, [0.02, 0.02]
 type_map = "map"
 
-map_request = f"http://static-maps.yandex.ru/1.x/?ll={lan},{lat}&spn={spn[0]},{spn[1]}&l={type_map}"
-response = requests.get(map_request)
-
-if not response:
-    print("Ошибка выполнения запроса:")
-    print(map_request)
-    print("Http статус:", response.status_code, "(", response.reason, ")")
-    sys.exit(1)
-
-map_file = "map.png"
-with open(map_file, "wb") as file:
-    file.write(response.content)
+get_map(lan, lat, spn, type_map)
 
 pygame.init()
 screen = pygame.display.set_mode((600, 450))
@@ -38,18 +43,7 @@ while run:
                 lat -= 1
             if event.key == pygame.K_LEFT:
                 lat += 1
-    map_request = f"http://static-maps.yandex.ru/1.x/?ll={lan},{lat}&spn={spn[0]},{spn[1]}&l={type_map}"
-    response = requests.get(map_request)
-
-    if not response:
-        print("Ошибка выполнения запроса:")
-        print(map_request)
-        print("Http статус:", response.status_code, "(", response.reason, ")")
-        sys.exit(1)
-
-    map_file = "map.png"
-    with open(map_file, "wb") as file:
-        file.write(response.content)
+            get_map(lan, lat, spn, type_map)
     screen.fill("black")
     screen.blit(pygame.image.load(map_file), (0, 0))
 pygame.quit()
