133f4f8e065169a3a51048ca9969a00960dcfa09
DCRIl <kelehsaev.2007@gmail.com>
2024-02-05 18:37:18 +0300

    сделал константами координаты и масштаб, тип карты

--- main.py
+++ main.py
@@ -4,8 +4,8 @@ import sys
 import pygame
 import requests
 
-lan, lat, spn = map(float, input().split())
-type_map = input()
+lan, lat, spn = 43.024616, 44.681771, [0.002, 0.002]
+type_map = "map"
 
 map_request = f"http://static-maps.yandex.ru/1.x/?ll={lan},{lat}&spn={spn[0]},{spn[1]}&l={type_map}"
 response = requests.get(map_request)
@@ -25,7 +25,11 @@ screen = pygame.display.set_mode((600, 450))
 screen.blit(pygame.image.load(map_file), (0, 0))
 pygame.display.flip()
 while pygame.event.wait().type != pygame.QUIT:
-    pass
+    for event in pygame.event.get():
+        if event.type == pygame.K_UP:
+            lan -= 0.01
+        if event.type == pygame.K_DOWN:
+            lan += 0.01
 pygame.quit()
 
 os.remove(map_file)
