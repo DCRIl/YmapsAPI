91cd52cdc3a681e3df8463731430cd81b4f29863
DCRIl <kelehsaev.2007@gmail.com>
2024-02-05 18:31:18 +0300

    добавил выбор координаты, масштаба и типа карты

--- main.py
+++ main.py
@@ -4,7 +4,10 @@ import sys
 import pygame
 import requests
 
-map_request = "http://static-maps.yandex.ru/1.x/?ll=37.530887,55.703118&spn=0.002,0.002&l=map"
+lan, lat, spn = map(float, input().split())
+type_map = input()
+
+map_request = f"http://static-maps.yandex.ru/1.x/?ll={lan},{lat}&spn={spn[0]},{spn[1]}&l={type_map}"
 response = requests.get(map_request)
 
 if not response:
@@ -13,21 +16,16 @@ if not response:
     print("Http статус:", response.status_code, "(", response.reason, ")")
     sys.exit(1)
 
-# Запишем полученное изображение в файл.
 map_file = "map.png"
 with open(map_file, "wb") as file:
     file.write(response.content)
 
-# Инициализируем pygame
 pygame.init()
 screen = pygame.display.set_mode((600, 450))
-# Рисуем картинку, загружаемую из только что созданного файла.
 screen.blit(pygame.image.load(map_file), (0, 0))
-# Переключаем экран и ждем закрытия окна.
 pygame.display.flip()
 while pygame.event.wait().type != pygame.QUIT:
     pass
 pygame.quit()
 
-# Удаляем за собой файл с изображением.
 os.remove(map_file)
