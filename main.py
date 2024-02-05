3a268d86c95075a545efdbfdbdd37c8f770f8bd9
DCRIl <kelehsaev.2007@gmail.com>
2024-02-05 20:10:42 +0300

    Добавил поле для ввода и кнопку и функцию отслеживающую нажатие кнопки

--- main.py
+++ main.py
@@ -2,43 +2,12 @@ import os
 import sys
 
 import pygame
+import pygame_gui
 import requests
 
 
 BLACK = "black"
 
-
-class TextInput:
-    def __init__(self, x, y, width, height, color=(255, 255, 255)):
-        self.rect = pygame.Rect(x, y, width, height)
-        self.color = color
-        self.text = ""
-        self.font = pygame.font.Font(None, 36)
-
-    def draw(self):
-        pygame.draw.rect(screen, self.color, self.rect)
-        text_surface = self.font.render(self.text, True, BLACK)
-        screen.blit(text_surface, (self.rect.x + 5, self.rect.y + 5))
-
-    def add_text(self, event):
-        if event.key == pygame.K_BACKSPACE:
-            self.text = self.text[:-1]
-        else:
-            self.text += event.unicode
-
-
-class Button:
-    def __init__(self, x, y, width, height, color, text):
-        self.rect = pygame.Rect(x, y, width, height)
-        self.color = color
-        self.text = text
-
-    def draw(self):
-        pygame.draw.rect(screen, self.color, self.rect)
-        font = pygame.font.Font(None, 36)
-        text = font.render(self.text, True, BLACK)
-        text_rect = text.get_rect(center=self.rect.center)
-        screen.blit(text, text_rect)
 
 
 def get_map(lan, lat, spn, type_map):
@@ -54,6 +23,12 @@ def get_map(lan, lat, spn, type_map):
     with open(map_file, "wb") as file:
         file.write(response.content)
 
+
+def btn_clk(event):
+    if event.user_type == pygame_gui.UI_BUTTON_PRESSED:
+        print("Press")
+
+
 map_file = "map.png"
 
 lan, lat, spn = 43.024616, 44.681771, [0.02, 0.02]
@@ -62,33 +37,43 @@ type_map = "map"
 get_map(lan, lat, spn, type_map)
 
 pygame.init()
-screen = pygame.display.set_mode((600, 450))
-screen.blit(pygame.image.load(map_file), (0, 0))
+screen = pygame.display.set_mode((600, 550))
+manager = pygame_gui.UIManager((600, 550))
+screen.blit(pygame.image.load(map_file), (0, 100))
 pygame.display.flip()
 run = True
+button = pygame_gui.elements.UIButton(relative_rect=pygame.Rect((450, 10, 140, 70)), text='Открыть', manager=manager)
+text_entry = pygame_gui.elements.UITextEntryLine(relative_rect=pygame.Rect((10, 10, 430, 70)), manager=manager)
+clock = pygame.time.Clock()
 while run:
+    time_delta = clock.tick(60) / 1000.0
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
+        if button.check_pressed():
+            print("Press")
         if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_9:
+            if event.key == pygame.K_PAGEUP:
                 spn[0] *= 2
                 spn[1] *= 2
                 get_map(lan, lat, spn, type_map)
-            if event.key == pygame.K_3:
+            if event.key == pygame.K_PAGEDOWN:
                 spn[0] /= 2
                 spn[1] /= 2
                 get_map(lan, lat, spn, type_map)
             if event.key == pygame.K_UP:
-                lan -= 0.005
+                lan -= 0.0005
             if event.key == pygame.K_DOWN:
-                lan += 0.005
+                lan += 0.0005
             if event.key == pygame.K_RIGHT:
-                lat += 0.005
+                lat += 0.0005
             if event.key == pygame.K_LEFT:
-                lat -= 0.005
+                lat -= 0.0005
             get_map(lan, lat, spn, type_map)
-    screen.blit(pygame.image.load(map_file), (0, 0))
+        manager.process_events(event)
+    manager.update(time_delta)
+    screen.blit(pygame.image.load(map_file), (0, 100))
+    manager.draw_ui(screen)
     pygame.display.flip()
 pygame.quit()
 
