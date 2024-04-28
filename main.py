e0da97ec7ade9fcb56fb6dd924a99ccd214efd7b
DCRIl <133270010+DCRIl@users.noreply.github.com>
2024-04-28 11:14:23 +0300

    Переделал на Qt

--- main.py
+++ main.py
@@ -1,80 +1,206 @@
-import os
-import sys
-
-import pygame
-import pygame_gui
 import requests
 
+from PyQt5.QtWidgets import QMainWindow, QMessageBox, QApplication
+from PyQt5 import uic
+from PyQt5.QtGui import QPixmap
+from PyQt5.QtCore import Qt
+from PyQt5 import QtGui
+from sys import argv, exit
 
-BLACK = "black"
+MAP_FILENAME = 'map.png'
+types = ['Схема', 'Спутник', 'Гибрид']
+types_map = {'Схема': 'map', 'Спутник': 'sat', 'Гибрид': 'sat,skl'}
+
+postal_types = ['Показывать почтовый индекс', 'Скрывать почтовый индекс']
+postal_types_map = {postal_types[0]: 'on', postal_types[1]: 'off'}
 
 
+def get_lonlat(search: str, postal_code: bool = True):
+    geocoder_apikey = "40d1649f-0493-4b70-98ba-98533de7710b"
+    url = f'https://geocode-maps.yandex.ru/1.x?geocode={search}&apikey={geocoder_apikey}&format=json'
+    response = requests.get(url)
+    json = response.json()
+    pos = json['response']['GeoObjectCollection']['featureMember'][0]['GeoObject']['Point']['pos']
+    lon, lat = [float(el) for el in pos.split()]
+    full_address = json['response']['GeoObjectCollection']['featureMember'][
+        0]['GeoObject']['metaDataProperty']['GeocoderMetaData']['text']
+    if postal_code == 'on':
+        code = json['response']['GeoObjectCollection']['featureMember'][
+            0]['GeoObject']['metaDataProperty']['GeocoderMetaData']['Address']['postal_code']
+        full_address += ' ' + code
+    return lon, lat, full_address
 
-def get_map(lan, lat, spn, type_map):
-    map_request = f"http://static-maps.yandex.ru/1.x/?ll={lat},{lan}&spn={spn[0]},{spn[1]}&l={type_map}"
-    response = requests.get(map_request)
 
-    if not response:
-        print("Ошибка выполнения запроса:")
-        print(map_request)
-        print("Http статус:", response.status_code, "(", response.reason, ")")
-        sys.exit(1)
-
-    with open(map_file, "wb") as file:
+def download_image(lon: float, lat: float, point_lon: float, point_lat: float, spn: float, type: str):
+    map_url = f'http://static-maps.yandex.ru/1.x/?ll={lon},{lat}&spn={spn},{spn}&l={type}'
+    if point_lon is not None and point_lat is not None:
+        map_url += f'&pt={point_lon},{point_lat},round'
+    response = requests.get(map_url)
+    with open(MAP_FILENAME, 'wb') as file:
         file.write(response.content)
 
 
-def btn_clk(event):
-    if event.user_type == pygame_gui.UI_BUTTON_PRESSED:
-        print("Press")
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        uic.loadUi('./main.ui', self)
+        self.spn = 0.02
+        self.point_lon, self.point_lat, full_address = get_lonlat(
+            'Москва')
+        self.set_full_address(full_address)
+        self.lon, self.lat = self.point_lon, self.point_lat
+        self.search_text = 'Москва'
+
+        self.type_box.clear()
+        self.type_box.addItems(types)
+        self.type_box.setCurrentIndex(0)
+        self.type_box.currentTextChanged.connect(self.type_changed)
+
+        self.postal_box.clear()
+        self.postal_box.addItems(postal_types)
+        self.postal_box.setCurrentIndex(0)
+        self.postal_box.currentTextChanged.connect(self.postal_changed)
+
+        self.update_map()
+        self.search_button.clicked.connect(self.search)
+        self.remove_button.clicked.connect(self.remove_point)
+
+        self.postal_code = 'on'
+
+    def update_map(self):
+        type_name = self.type_box.currentText()
+        type = types_map.get(type_name, 'map')
+        download_image(self.lon, self.lat, self.point_lon,
+                       self.point_lat, self.spn, type)
+        pixmap = QPixmap(MAP_FILENAME)
+        self.image_label.setPixmap(pixmap)
+
+    def keyPressEvent(self, event):
+        if event.key() == Qt.Key_PageDown:
+            self.spn = self.spn * 2
+            self.update_map()
+        elif event.key() == Qt.Key_PageUp:
+            self.spn = self.spn / 2
+            self.update_map()
+        elif event.key() == Qt.Key_Left:
+            self.lon -= self.spn / 4
+            self.update_map()
+        elif event.key() == Qt.Key_Right:
+            self.lon += self.spn / 4
+            self.update_map()
+        elif event.key() == Qt.Key_Up:
+            self.lat += self.spn / 4
+            self.update_map()
+        elif event.key() == Qt.Key_Down:
+            self.lat -= self.spn / 4
+            self.update_map()
+
+    def mousePressEvent(self, event):
+        self.remove_focus()
+        # try:
+        button = event.button()
+        if button == 1:
+            pos = event.pos()
+            x = pos.x()
+            y = pos.y()
+            left_edge_lon = self.lon - ((self.spn / 2) * (650 / 255))
+            top_edge_lat = self.lat + (self.spn / 2)
+            y += 10
+            search_lon = left_edge_lon + (x / 650 * self.spn * (650 / 240))
+            search_lat = top_edge_lat - (y / 400 * self.spn)
+
+            geocoder_apikey = "40d1649f-0493-4b70-98ba-98533de7710b"
+            url = f'https://geocode-maps.yandex.ru/1.x?geocode={search_lon},{search_lat}&apikey={geocoder_apikey}&format=json'
+            response = requests.get(url)
+            json = response.json()
+            pos = json['response']['GeoObjectCollection']['featureMember'][0]['GeoObject']['Point']['pos']
+            try:
+                full_address = json['response']['GeoObjectCollection']['featureMember'][
+                    0]['GeoObject']['metaDataProperty']['GeocoderMetaData']['text']
+                search_text = full_address
+                if self.postal_code == 'on':
+                    code = json['response']['GeoObjectCollection']['featureMember'][
+                        0]['GeoObject']['metaDataProperty']['GeocoderMetaData']['Address']['postal_code']
+                    full_address += ' ' + code
+                self.set_full_address(full_address)
+                self.search_text = search_text
+            except:
+                pass
+            self.point_lon = search_lon
+            self.point_lat = search_lat
+            self.update_map()
+        elif button == 2:
+            pos = event.pos()
+            x = pos.x()
+            y = pos.y()
+            left_edge_lon = self.lon - ((self.spn / 2) * (650 / 255))
+            top_edge_lat = self.lat + (self.spn / 2)
+            y += 10
+            search_lon = left_edge_lon + (x / 650 * self.spn * (650 / 240))
+            search_lat = top_edge_lat - (y / 400 * self.spn)
+
+            ppo_apikey = '73961a13-a537-4463-a34a-bff0205a48e8'
+            geocoder_apikey = "40d1649f-0493-4b70-98ba-98533de7710b"
+            try:
+                url = f'https://geocode-maps.yandex.ru/1.x?geocode={search_lon},{search_lat}&apikey={geocoder_apikey}&format=json'
+                response = requests.get(url)
+                json = response.json()
+                pos = json['response']['GeoObjectCollection']['featureMember'][0]['GeoObject']['Point']['pos']
+                full_address = json['response']['GeoObjectCollection']['featureMember'][
+                    0]['GeoObject']['metaDataProperty']['GeocoderMetaData']['text']
+
+                url = f'https://search-maps.yandex.ru/v1/?lang=ru_RU&apikey={ppo_apikey}&text={full_address}&type=biz&ll={search_lon},{search_lat}'
+                response = requests.get(url).json()
+                print(response)
+                result = response['features'][0]['properties']['name']
+
+                QMessageBox.information(self, 'Результат поиска', result)
+            except:
+                pass
+
+    def remove_focus(self, *args):
+        try:
+            focused_widget = QApplication.focusWidget()
+            focused_widget.clearFocus()
+        except Exception as e:
+            print(e)
+
+    def type_changed(self, event):
+        self.update_map()
+
+    def search(self, event):
+        search_text = self.search_input.text().lower()
+        try:
+            lon, lat, full_address = get_lonlat(search_text, self.postal_code)
+            self.set_full_address(full_address)
+            self.search_text = search_text
+            self.lon = lon
+            self.lat = lat
+            self.point_lon = lon
+            self.point_lat = lat
+            self.spn = 0.02
+            self.update_map()
+        except Exception:
+            # по запросу ничего не нашлось
+            pass
+
+    def remove_point(self, event):
+        self.point_lat = None
+        self.point_lon = None
+        self.update_map()
+        self.set_full_address('')
+
+    def set_full_address(self, full_address: str):
+        self.address_label.setText(full_address)
+
+    def postal_changed(self, event):
+        postal_type_text = self.postal_box.currentText()
+        self.postal_code = postal_types_map.get(postal_type_text, 'on')
+        self.search(None)
 
 
-map_file = "map.png"
-
-lan, lat, spn = 43.024616, 44.681771, [0.02, 0.02]
-type_map = "map"
-
-get_map(lan, lat, spn, type_map)
-
-pygame.init()
-screen = pygame.display.set_mode((600, 550))
-manager = pygame_gui.UIManager((600, 550))
-screen.blit(pygame.image.load(map_file), (0, 100))
-pygame.display.flip()
-run = True
-button = pygame_gui.elements.UIButton(relative_rect=pygame.Rect((450, 10, 140, 70)), text='Открыть', manager=manager)
-text_entry = pygame_gui.elements.UITextEntryLine(relative_rect=pygame.Rect((10, 10, 430, 70)), manager=manager)
-clock = pygame.time.Clock()
-while run:
-    time_delta = clock.tick(60) / 1000.0
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            run = False
-        if button.check_pressed():
-            print("Press")
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_PAGEUP:
-                spn[0] *= 2
-                spn[1] *= 2
-                get_map(lan, lat, spn, type_map)
-            if event.key == pygame.K_PAGEDOWN:
-                spn[0] /= 2
-                spn[1] /= 2
-                get_map(lan, lat, spn, type_map)
-            if event.key == pygame.K_UP:
-                lan -= 0.0005
-            if event.key == pygame.K_DOWN:
-                lan += 0.0005
-            if event.key == pygame.K_RIGHT:
-                lat += 0.0005
-            if event.key == pygame.K_LEFT:
-                lat -= 0.0005
-            get_map(lan, lat, spn, type_map)
-        manager.process_events(event)
-    manager.update(time_delta)
-    screen.blit(pygame.image.load(map_file), (0, 100))
-    manager.draw_ui(screen)
-    pygame.display.flip()
-pygame.quit()
-
-os.remove(map_file)
+if __name__ == '__main__':
+    app = QApplication(argv)
+    main_window = MainWindow()
+    main_window.show()
+    exit(app.exec())
